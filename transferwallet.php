cd74f4696d9eb7d587c2faa805acf2aab7036de6
Joe <66766658+ghost9425@users.noreply.github.com>
2022-02-03 17:52:28 +0700

    edit

--- transferwallet.php
+++ transferwallet.php
@@ -2936,7 +2936,7 @@
                             <span class="hljs-attr">"agentPt"</span>: <span class="hljs-number">0</span>
                             }
                         ]
-                    },
+                    ],
                     <span class="hljs-attr">"drawResult"</span>: {
                         <span class="hljs-attr">"bottom1"</span>: [
                             <span class="hljs-literal">"5"</span>,
@@ -3077,10 +3077,11 @@
                                 </div>
                                 <div class="koh-faq-answer col-9">
                                     <pre><code id="49A" style="border-radius: 0.375rem;" class="hljs json">{
-    <span class="hljs-attr">"agentUsername"</span>: <span class="hljs-string">"agentapi",</span>
+    <span class="hljs-attr">"agentUsername"</span>: <span class="hljs-string">"company",</span>
     <span class="hljs-attr">"agentApiKey"</span>: <span class="hljs-string">"18c089a2e49dfc1776b6abc2ea3ee909d43b10bc37dfd7195a7265da82f8b4a983a1ebf3",</span>
     <span class="hljs-attr">"startDate"</span>: <span class="hljs-string">"2021-07-14 15:04:05",</span>
     <span class="hljs-attr">"endDate"</span>: <span class="hljs-string">"2021-07-14 15:04:05",</span>
+    <span class="hljs-attr">"ticketId"</span>: <span class="hljs-string">91565</span>,
     <span class="hljs-attr">"page"</span>: <span class="hljs-number">1,</span>
     <span class="hljs-attr">"limit"</span>: <span class="hljs-number">1</span>
 }</code></pre>
